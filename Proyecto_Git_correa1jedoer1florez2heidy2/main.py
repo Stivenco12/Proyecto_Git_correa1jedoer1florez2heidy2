0903c4d0ebb70eb188c7c5b8a70b4b375a9fb36d
lisethflorez <lisethflorez81@gmail.com>
2024-08-16 15:34:57 -0500

    feat:terminacion de menu main

---
+++ Proyecto_Git_correa1jedoer1florez2heidy2/main.py
@@ -0,0 +1,31 @@
+import os
+import modulos.funcionalidad
+def menuprincipal(op : int):
+    title = """
+    *******************************************
+    *  GESTIONAMIENTOS DE LAS OPERACIONES 
+                 EN  SUCURSALES               *
+    *******************************************
+    """
+    menuprincipal = '1.ingresar sucursal \n2.editar datos de la sucursal\n3.visualisar sucursales \n4. Salir'
+    if (op != 4):
+        print(title)
+        print(menuprincipal)
+        try:
+            op = int(input(":) "))
+        except ValueError:
+            print("Opcion no tiene formato adecuado")
+            menuprincipal(0)
+        else:
+            match (op):
+                case 1:
+                   function.añadido(0)
+                case 2:
+                    (0)
+                case 3:
+                    uic.menucitas(0)
+                case _:
+                    print()
+
+menuprincipal(1)
+     
---
+++ Proyecto_Git_correa1jedoer1florez2heidy2/visualisar.py
@@ -0,0 +1,28 @@
+import os
+import modulos.funcionalidad
+def menuprincipal(op : int):
+    title = """
+    *******************************************
+    *       VISUALISACIONES SUCURSALES       *
+    *******************************************
+    """
+    menuStudentOp = '1.ingresar sucursal \n2.editar datos de la sucursal\n3.visualisar sucursales \n4. Salir'
+    if (op != 4):
+        print(title)
+        print(menuStudentOp)
+        try:
+            op = int(input(":) "))
+        except ValueError:
+            print("Opcion no tiene formato adecuado")
+            menuprincipal(0)
+        else:
+            match (op):
+                case 1:
+                   function.añadido(0)
+                case 2:
+                    (0)
+                case 3:
+                    (0)
+                case _:
+                    print()
+                    menuprincipal
